e3353e777cab33cf6a92e0681118f5d63be7245c
Juliendy Mendes <juliendy0708@gmail.com>
2024-05-13 21:13:02 -0400

    feat: styles for form inputs and search results cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgIf, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'desafio-linked';
+  name = '';
+  block = "";
+  list = [1,2,3,4,5,6,7,8,9,10]
+  buscar(){
+    console.log(this.name);
+    console.log(this.block);
+
+  }
 }
