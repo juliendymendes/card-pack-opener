49ad9e75d8d4c7fc3c520d7d1debbd6294271c08
Juliendy Mendes <juliendy0708@gmail.com>
2024-05-14 11:15:49 -0400

    feat: display results on screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,26 @@
-import { NgFor, NgIf } from '@angular/common';
+import { DatePipe, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-
+import { ApiService } from './shared/services/api.service';
+import { ISet } from './shared/types/Set';
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, FormsModule, NgIf, NgFor],
-  templateUrl: './app.component.html',
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet, FormsModule, NgIf, NgFor, DatePipe],
+	templateUrl: './app.component.html',
 })
 export class AppComponent {
-  name = '';
-  block = "";
-  list = [1,2,3,4,5,6,7,8,9,10]
-  buscar(){
-    console.log(this.name);
-    console.log(this.block);
-
-  }
+	name = '';
+	block = '';
+	blocks = ['Amonkhet', 'Ixalan', 'Zendikar', 'Ravnica', 'Onslaught'];
+	sets: ISet[] = [];
+	constructor(private apiService: ApiService) {}
+	search() {
+    if(this.block != ''){
+      this.apiService.getSets(this.name, this.block).subscribe((sets) => {
+        this.sets = sets.sets;
+      });
+    }
+	}
 }
