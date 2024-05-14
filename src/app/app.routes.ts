b6966dc94a4258deb2779c8bcec31e774a5fb336
Juliendy Mendes <juliendy0708@gmail.com>
2024-05-14 12:25:28 -0400

    feat: style for booters cards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { BoosterCardsComponent } from './booster-cards/booster-cards.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "cards", component: BoosterCardsComponent}
+];
