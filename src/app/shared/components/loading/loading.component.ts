a8a6873b1712e92b381eb9bf462b45113222239b
Juliendy Mendes <juliendy0708@gmail.com>
2024-05-14 12:25:00 -0400

    feat: loading component

---
+++ src/app/shared/components/loading/loading.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'loading',
+  standalone: true,
+  imports: [],
+  templateUrl: './loading.component.html',
+})
+export class LoadingComponent {
+
+}
